2fc2c19c738dcfb6caba7d929dfa1e8bdd2c212c
spacercoding <pinheirojordan106@gmail.com>
2026-03-03 20:15:46 +0000

    aula1

--- ED/sandbox/00_rascunhos/pintando/draft.go
+++ ED/sandbox/00_rascunhos/pintando/draft.go
@@ -1,5 +1,28 @@
 package main
-import "fmt"
+
+import (
+    "fmt"
+    "math"
+)
+
+func hedron(lado1, lado2, lado3 int) (float32) {
+
+    var semiPerimetro, area float32
+
+    semiPerimetro = (lado1 + lado2 + lado3) / 2
+
+    area = math.Pow(2, semiPerimetro * (semiPerimetro - lado1) * (semiPerimetro - lado2) * (semiPerimetro - lado3))
+
+    return area
+
+}
+
 func main() {
-    fmt.Println("qxcode")
+
+    var lado1, lado2, lado3 int
+
+    fmt.Scan(&lado1, &lado2, &lado3)
+
+    hedron(lado1, lado2, lado3)
+
 }
