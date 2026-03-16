28ae1c4ae4df079bc4db2fab891de6ca5af8386b
spacercoding <pinheirojordan106@gmail.com>
2026-03-16 19:17:26 +0000

    baruel

--- ED/ed/baruel/go/draft.go
+++ ED/ed/baruel/go/draft.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func atribuirInteiro(inteiro *int) {
 
@@ -12,7 +16,7 @@ func preencherAlbum(album map[int]int, figObtidas, figTotal int) {
 
 	var figurinha int
 
-	for i := range figTotal {
+	for i := 1; i <= figTotal; i++ {
 
 		album[i] = 0
 
@@ -28,15 +32,50 @@ func preencherAlbum(album map[int]int, figObtidas, figTotal int) {
 
 }
 
-func analisarAlbum(album map[int]int) {
+func analisarAlbum(album map[int]int, figTotal int) {
 
-	for i := range album {
+	var repetidas = make([]int, 0)
+	var faltando = make([]int, 0)
 
-		if album[i] > 0 {
+	for i := 1; i < figTotal+1; i++ {
+
+		if album[i] > 1 {
+			for j := 0; j < album[i]-1; j++ {
+				repetidas = append(repetidas, i)
+			}
+
+		} else if album[i] == 0 {
+
+			faltando = append(faltando, i)
 
 		}
 
 	}
+	if len(repetidas) > 0 {
+		fmt.Println(strings.Join(formatarVetor(repetidas), " "))
+	} else {
+		fmt.Println("N")
+	}
+
+	if len(faltando) > 0 {
+		fmt.Println(strings.Join(formatarVetor(faltando), " "))
+	} else {
+		fmt.Println("N")
+	}
+
+}
+
+func formatarVetor(vetor []int) []string {
+
+	var vetorFormatado = make([]string, 0)
+
+	for i := range vetor {
+
+		vetorFormatado = append(vetorFormatado, strconv.Itoa(vetor[i]))
+
+	}
+
+	return vetorFormatado
 
 }
 
@@ -49,6 +88,6 @@ func main() {
 	var album = make(map[int]int, figObtidas)
 	preencherAlbum(album, figObtidas, figTotal)
 
-	analisarAlbum(album)
+	analisarAlbum(album, figTotal)
 
 }
