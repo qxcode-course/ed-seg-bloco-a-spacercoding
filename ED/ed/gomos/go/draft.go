e21ee077449e41008f848a7b0fc8e28aaab53dab
spacercoding <pinheirojordan106@gmail.com>
2026-03-16 19:52:24 +0000

    cobra*

--- ED/ed/gomos/go/draft.go
+++ ED/ed/gomos/go/draft.go
@@ -1,5 +1,77 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
+type posGomo struct {
+	x, y int
+}
+
+func atribuirInt(meuInt *int) {
+
+	fmt.Scan(meuInt)
+
+}
+
+func atribuirString(minhaRune *string) {
+
+	fmt.Scan(minhaRune)
+
+}
+
+func atribuirVetor(cobra []posGomo) {
+
+	for i := range cobra {
+
+		fmt.Scan(&cobra[i].x, &cobra[i].y)
+
+	}
+
+}
+
+func atualizarPos(cobra []posGomo, direcao string) {
+
+	var posTemp posGomo
+
+	if direcao == "L" {
+
+		posTemp = cobra[0]
+		cobra[0].x--
+
+	}
+	if direcao == "R" {
+
+		cobra[0].x++
+
+	}
+	if direcao == "U" {
+
+		cobra[0].y--
+
+	}
+	if direcao == "D" {
+
+		cobra[0].y++
+
+	}
+	for i := range cobra {
+		fmt.Printf("%d %d\n", cobra[i].x, cobra[i].y)
+	}
+
+}
+
 func main() {
-    fmt.Println("qxcode")
+
+	var qtdGomos int
+	atribuirInt(&qtdGomos)
+
+	var direcao string
+	atribuirString(&direcao)
+
+	var cobra = make([]posGomo, qtdGomos)
+	atribuirVetor(cobra)
+
+	atualizarPos(cobra, direcao)
+
 }
